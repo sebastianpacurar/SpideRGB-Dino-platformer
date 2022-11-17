4f5f86da614c0be408a9f9eb45ccd53362d5f139
sebastianpacurar <pacurar.sebastian90@gmail.com>
2022-11-17 02:53:33 +0200

    improve menus: - add proper sprites for containers, buttons etc - improve navigation

--- Assets/Scripts/Menu/InGameMenu/PauseMenu.cs
+++ Assets/Scripts/Menu/InGameMenu/PauseMenu.cs
@@ -1,19 +1,17 @@
 using Player;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 namespace Menu.InGameMenu {
     public class PauseMenu : MonoBehaviour {
+        [SerializeField] private GameObject panel;
         private PlayerControls _controls;
+
         private bool _isPaused;
-        private GameObject _panel;
 
         private void Awake() {
             _controls = new PlayerControls();
-        }
-
-        private void Start() {
-            _panel = GameObject.FindGameObjectWithTag("PauseMenu").gameObject;
         }
 
         private void ToggleMenu(InputAction.CallbackContext ctx) {
@@ -27,13 +25,18 @@ namespace Menu.InGameMenu {
 
         private void PauseMenuActivate() {
             Time.timeScale = 0;
-            _panel.SetActive(true);
+            panel.SetActive(true);
         }
 
         //TODO: used by Back button
         public void PauseMenuDeactivate() {
             Time.timeScale = 1;
-            _panel.SetActive(false);
+            panel.SetActive(false);
+        }
+
+        public void GoToMainMenu() {
+            Time.timeScale = 1;
+            SceneManager.LoadScene("MainMenu");
         }
 
         private void OnEnable() {
